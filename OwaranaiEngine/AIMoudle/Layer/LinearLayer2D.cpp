f53aa10e3f4307f47b494dd5a7740e48350f199b
takahashiuhiro <takahashiuhiro@gmail.com>
2022-12-06 00:32:11 +0800

    fix a bug

--- OwaranaiEngine/AIMoudle/Layer/LinearLayer2D.cpp
+++ OwaranaiEngine/AIMoudle/Layer/LinearLayer2D.cpp
@@ -3,6 +3,6 @@
 void LinearLayer2D::ForwardBuild()
 {
     CGNode *Intermediate_1 = new CGNode(std::vector<CGNode*>{ParamsCGNode[0],InputCGNode[0]},"Matmul", 1);
-    CGNode *Intermediate_2 = new CGNode(std::vector<CGNode*>{Intermediate_1,ParamsCGNode[0]},"Matmul", 1);
+    CGNode *Intermediate_2 = new CGNode(std::vector<CGNode*>{Intermediate_1,ParamsCGNode[1]},"Matmul", 1);
     ForwardNode = Intermediate_2;
 }
