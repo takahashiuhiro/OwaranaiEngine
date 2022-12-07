71baece8933218a31b8d1d3ccd92c79547753d17
takahashiuhiro <takahashiuhiro@gmail.com>
2022-12-07 23:56:23 +0800

    update loss

--- OwaranaiEngine/AIMoudle/LossFunction/MSELoss.h
+++ OwaranaiEngine/AIMoudle/LossFunction/MSELoss.h
@@ -6,7 +6,18 @@ struct MSELoss:BaseLoss
 public:
     virtual void LossBuild()
     {
-        //todo::等下补好loss，还得写个减法算子..
-        //CGNode *Intermediate_1 = new CGNode(std::vector<CGNode*>{OutputNode[0],LabelNode[0]},"Matmul", 1);
+        //建立一个常量-1矩阵和想减掉的矩阵元素乘
+        Tensor* Constant = new Tensor(OutputNode[0]->NodeContent->shape, OutputNode[0]->NodeContent->Device, OutputNode[0]->NodeContent->DeviceNum);
+        Constant->FillArray(-1.);
+        CGNode *ConstantNode = new CGNode(Constant, 0);
+        ConstantNode->NodeType["Constant"] = 1;
+        CGNode *Intermediate_0 = new CGNode(std::vector<CGNode*>{LabelNode[0], ConstantNode},"EleMul", 1);
+        //用输出节点减掉标准节点
+        CGNode *Intermediate_1 = new CGNode(std::vector<CGNode*>{OutputNode[0],Intermediate_0},"Add", 1);
+        //复制一个节点
+        CGNode *Intermediate_2 = new CGNode(std::vector<CGNode*>{Intermediate_1},"Add", 1);
+        //两个节点相乘
+        CGNode *Intermediate_3 = new CGNode(std::vector<CGNode*>{Intermediate_1, Intermediate_2},"EleMul", 1);
+        LossNode = Intermediate_3;
     }
 };
--- OwaranaiEngine/AIMoudle/LossFunction/MoudleInclude.h
+++ OwaranaiEngine/AIMoudle/LossFunction/MoudleInclude.h
@@ -1,1 +1,3 @@
 #pragma once
+#include "BaseLoss.h"
+#include "MSELoss.h"
