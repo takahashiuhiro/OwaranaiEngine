0054c05ff524b660cbc84a06562207bee05a9612
takahashiuhiro <takahashiuhiro@gmail.com>
2023-01-31 00:01:26 +0800

    注释中文后面记得加句点，不然高贵的vs要程序的狗命

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "OwaranaiEngine/OwaranaiEngineInclude.h"
 int main()
 { 
-    std::cout<<"------------------------CPU test---------------------------------."<<std::endl;
+    std::cout<<"------------------------GPU test---------------------------------."<<std::endl;
     /**创建一个线性模块.*/
     LinearLayer2D* LinearBlock = new LinearLayer2D();
     /**参数输入.*/
@@ -13,14 +13,14 @@ int main()
     std::vector<size_t>*InputShape = (LinearBlock->Params).Get<std::vector<size_t>>("InputShape");
     std::vector<size_t>*OutputShape = (LinearBlock->Params).Get<std::vector<size_t>>("OutputShape");
     /**读取输入矩阵.*/
-    Tensor* q =new Tensor(std::vector<size_t>{BatchSize,(*InputShape)[0],(*InputShape)[1]}, "CPU", 0);
+    Tensor* q =new Tensor(std::vector<size_t>{BatchSize,(*InputShape)[0],(*InputShape)[1]}, "GPU", 0);
     q->FillArray(3.);
     CGNode *tuq = new CGNode(q, 1);
     /**初始化参数矩阵.*/
-    Tensor* w =new Tensor(std::vector<size_t>{BatchSize,(*OutputShape)[0],(*InputShape)[0]}, "CPU", 0);
+    Tensor* w =new Tensor(std::vector<size_t>{BatchSize,(*OutputShape)[0],(*InputShape)[0]}, "GPU", 0);
     w->FillArray(5.);
     CGNode *tuw = new CGNode(w, 1);
-    Tensor* e =new Tensor(std::vector<size_t>{BatchSize,(*InputShape)[1],(*OutputShape)[1]}, "CPU", 0);
+    Tensor* e =new Tensor(std::vector<size_t>{BatchSize,(*InputShape)[1],(*OutputShape)[1]}, "GPU", 0);
     e->FillArray(7.);
     CGNode *tue = new CGNode(e, 1);
     /**为网络设置输入矩阵和参数矩阵.*/
@@ -28,11 +28,11 @@ int main()
     LinearBlock->ParamsCGNode = std::vector<CGNode*>{tuw, tue};
     //设置loss.
     LinearBlock->SetLossFunction("MSE");
-    Tensor* LabelTensor =new Tensor(std::vector<size_t>{BatchSize,(*OutputShape)[0],(*OutputShape)[1]}, "CPU", 0);
+    Tensor* LabelTensor =new Tensor(std::vector<size_t>{BatchSize,(*OutputShape)[0],(*OutputShape)[1]}, "GPU", 0);
     LabelTensor->FillArray(3149.);
     CGNode* LabelNode = new CGNode(LabelTensor, 0);
     /**先得到前向的结果才能构建loss，因为loss运算需要前向的结果.*/
-    /**前向图构建*/
+    /**前向图构建.*/
     LinearBlock->ForwardBuild();
     /**执行前向运算.*/
     LinearBlock->Forward();
