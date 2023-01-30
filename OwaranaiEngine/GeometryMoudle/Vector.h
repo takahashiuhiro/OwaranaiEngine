16017145f3fffb25842fe6a36da923d43dd4276a
takahashiuhiro <takahashiuhiro@gmail.com>
2023-01-31 00:21:21 +0800

    update restart geo moudle

--- OwaranaiEngine/GeometryMoudle/Vector.h
+++ OwaranaiEngine/GeometryMoudle/Vector.h
@@ -21,17 +21,17 @@ public:
 
 	double X, Y, Z;
 
-	/**重载基本运算*/
+	/**重载基本运算.*/
 	Vector operator + (const Vector& Right) { return Vector(X + Right.X, Y + Right.Y, Z + Right.Z); }
 	Vector operator - (const Vector& Right) { return Vector(X - Right.X, Y - Right.Y, Z - Right.Z); }
 	Vector operator * (const double& Right) { return Vector(X * Right, Y * Right, Z * Right); }
 	double operator * (const Vector& Right) { return (X * Right.X) + (Y * Right.Y) + (Z * Right.Z); }
 	Vector operator / (const double& Right) { return Vector(X / Right, Y / Right, Z/Right); }
 
-	/**向量长度*/
+	/**向量长度.*/
 	double Length() { return sqrt(X * X + Y * Y + Z * Z); }
 
-	/**单位向量*/
+	/**单位向量.*/
 	Vector DirectionVector()
 	{
 		double VectorLength = Length();
@@ -39,9 +39,9 @@ public:
 		return *this / VectorLength;
 	}
 
-	/**向量叉乘*/
+	/**向量叉乘.*/
 	Vector CrossProduct(Vector Right) { return Vector(Y * Right.Z - Right.Y * Z, Right.X * Z - X * Right.Z, X * Right.Y - Right.X * Y); }
 
-	/**打印数据*/
+	/**打印数据.*/
 	void PrintData() {std::cout << "X:" << X << " X:" << Y << " Z:"<<Z << std::endl;}
 };
--- OwaranaiEngine/TestFile/LinearNetTest.cpp
+++ OwaranaiEngine/TestFile/LinearNetTest.cpp
@@ -1,48 +1,48 @@
 #include "OwaranaiEngine/OwaranaiEngineInclude.h"
 int main()
-{
-    std::cout<<"------------------------GPU test---------------------------------"<<std::endl;
-    /**创建一个线性模块*/
+{ 
+    std::cout<<"------------------------GPU test---------------------------------."<<std::endl;
+    /**创建一个线性模块.*/
     LinearLayer2D* LinearBlock = new LinearLayer2D();
-    /**参数输入*/
-    LinearBlock->Params.Set("InputShape", 3, std::vector<size_t>{5,6});
-    LinearBlock->Params.Set("OutputShape", 3, std::vector<size_t>{1,8});
-    LinearBlock->Params.Set("BatchSize", 3, std::vector<size_t>{2});
-    /**参数读取*/
+    /**参数输入.*/
+    LinearBlock->Params.Set("InputShape",  std::vector<size_t>{5,6});
+    LinearBlock->Params.Set("OutputShape",  std::vector<size_t>{1,8});
+    LinearBlock->Params.Set("BatchSize",  std::vector<size_t>{2});
+    /**参数读取.*/
     size_t BatchSize = (*(LinearBlock->Params).Get<std::vector<size_t>>("BatchSize"))[0];
     std::vector<size_t>*InputShape = (LinearBlock->Params).Get<std::vector<size_t>>("InputShape");
     std::vector<size_t>*OutputShape = (LinearBlock->Params).Get<std::vector<size_t>>("OutputShape");
-    /**读取输入矩阵*/
+    /**读取输入矩阵.*/
     Tensor* q =new Tensor(std::vector<size_t>{BatchSize,(*InputShape)[0],(*InputShape)[1]}, "GPU", 0);
     q->FillArray(3.);
     CGNode *tuq = new CGNode(q, 1);
-    /**初始化参数矩阵*/
+    /**初始化参数矩阵.*/
     Tensor* w =new Tensor(std::vector<size_t>{BatchSize,(*OutputShape)[0],(*InputShape)[0]}, "GPU", 0);
     w->FillArray(5.);
     CGNode *tuw = new CGNode(w, 1);
     Tensor* e =new Tensor(std::vector<size_t>{BatchSize,(*InputShape)[1],(*OutputShape)[1]}, "GPU", 0);
     e->FillArray(7.);
     CGNode *tue = new CGNode(e, 1);
-    /**为网络设置输入矩阵和参数矩阵*/
+    /**为网络设置输入矩阵和参数矩阵.*/
     LinearBlock->InputCGNode = std::vector<CGNode*>{tuq};
     LinearBlock->ParamsCGNode = std::vector<CGNode*>{tuw, tue};
-    //设置loss
+    //设置loss.
     LinearBlock->SetLossFunction("MSE");
     Tensor* LabelTensor =new Tensor(std::vector<size_t>{BatchSize,(*OutputShape)[0],(*OutputShape)[1]}, "GPU", 0);
     LabelTensor->FillArray(3149.);
     CGNode* LabelNode = new CGNode(LabelTensor, 0);
-    /**先得到前向的结果才能构建loss，因为loss运算需要前向的结果*/
-    /**前向图构建*/
+    /**先得到前向的结果才能构建loss，因为loss运算需要前向的结果.*/
+    /**前向图构建.*/
     LinearBlock->ForwardBuild();
-    /**执行前向运算*/
+    /**执行前向运算.*/
     LinearBlock->Forward();
-    /**输入loss所需的参数*/
+    /**输入loss所需的参数.*/
     LinearBlock->LossFunctionPointer->OutputNode.push_back(LinearBlock->ForwardNode);
     LinearBlock->LossFunctionPointer->LabelNode.push_back(LabelNode);
     LinearBlock->LossFunctionPointer->LossBuild();
-    /**执行前向(训练版)*/
+    /**执行前向(训练版).*/
     LinearBlock->LossFunctionPointer->LossNode->Forward();
-    /**执行反向*/
+    /**执行反向.*/
     LinearBlock->Backward(LinearBlock->LossFunctionPointer->LossNode->NodeContent);
 
     SGDOptimizer TestSGD = SGDOptimizer(0.001, SGDOptimizerInputTypeConst::BY_CGNODE);
@@ -68,6 +68,4 @@ int main()
         //LinearBlock->ParamsCGNode[a]->DerivativeNode->NodeContent->PrintData();
         LinearBlock->ParamsCGNode[a]->NodeContent->PrintData();
     }
-
-
 }
