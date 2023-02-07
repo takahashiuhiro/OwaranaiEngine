005139ed5b95245bc8a1404960c7b950499b1339
takahashiuhiro <takahashiuhiro@gmail.com>
2023-02-08 00:29:42 +0800

    update plane

--- OwaranaiEngine/GeometryMoudle/Plane.h
+++ OwaranaiEngine/GeometryMoudle/Plane.h
@@ -11,20 +11,36 @@ public:
     {
         this->AnchorPosition = AnchorPosition;
         this->NormalVector = NormalVector;
+        double MapLen = NormalVector*Vector(0,0,1);
         Vector RandomPositionOnPlane;
-        if(NormalVector.Z*NormalVector.Z < 1e-8)
+        if((MapLen - NormalVector.Length())*(MapLen - NormalVector.Length()) < 1e-8)
         {
-            
+            RandomPositionOnPlane = AnchorPosition + Vector(0,1,0);
         }
         else
         {
-
+            RandomPositionOnPlane = AnchorPosition + Vector(0,0,1);
+        }
+        Vector VectorOnPlane = VectorProjectionToPlane(RandomPositionOnPlane).DirectionVector();
+        Vector AnotherVectorOnPlane = VectorOnPlane.CrossProduct(NormalVector).DirectionVector();
+        if(VectorOnPlane.CrossProduct(AnotherVectorOnPlane)*NormalVector > 0)
+        {
+            BaseVector[0] = VectorOnPlane;
+            BaseVector[1] = AnotherVectorOnPlane;
+        }
+        else
+        {
+            BaseVector[0] = AnotherVectorOnPlane;
+            BaseVector[1] = VectorOnPlane;
         }
     }
 
-    Plane(Vector BaseVectorFirst, Vector BaseVectorSecond)
+    Plane(Vector AnchorPosition, Vector BaseVectorFirst, Vector BaseVectorSecond)
     {
-
+        this->AnchorPosition = AnchorPosition;
+        BaseVector[0] = BaseVectorFirst.DirectionVector();
+        BaseVector[1] = BaseVectorSecond.DirectionVector();
+        this->NormalVector = BaseVector[0].CrossProduct(BaseVector[1]);
     }
 
     /**锚定点.*/
@@ -33,4 +49,14 @@ public:
     Vector NormalVector = Vector(0,1,0);
     /**平面上当前的正交基.*/
     Vector BaseVector[2] = {Vector(0,1,0), Vector(1,0,0)};
+
+    /**
+     * 空间中一点在平面上的投影
+     * Params:空间中的一个点
+    */
+    Vector VectorProjectionToPlane(Vector PositionA)
+    {
+        double FromAnchorToPositionA = (PositionA - AnchorPosition)*NormalVector;
+        return PositionA - NormalVector*FromAnchorToPositionA;
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,14 +1,5 @@
 #include "OwaranaiEngine/OwaranaiEngineInclude.h"
 int main()
 { 
-    Segment e = Segment(Vector(0,0), Vector(10,0));
-    Segment q = Segment(Vector(10,1), Vector(0,-9));
-    Vector Res;
-    if(SegmentCross2D(e,q,Res))
-    {
-        Res.PrintData();
-    }
-
-    Line qwe;
-    Plane ewq;
+    Plane(Vector(), Vector(0,1), Vector(1,0)).NormalVector.PrintData();
 }
