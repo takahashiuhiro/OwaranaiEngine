4dc3936efd8342c78208324326d11daa4542d350
takahashiuhiro <takahashiuhiro@outlook.com>
2023-09-24 02:52:09 +0800

    add layer

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@
 int main() 
 {
     Tensor* q = new Tensor({2,5,4}, 0);
-    //Tensor* w = new Tensor({1,2,5}, 0);
+    Tensor* w = new Tensor({1,2,5}, 0);
     q->FillArray(12);
     std::string qq = "ww.ee";
-    //w->FillArray(1);
-    //w->SaveToFile(qq);
+    w->FillArray(1);
+    w->SaveToFile(qq);
 
-    Tensor* w = Tensor::CreateTensorByLoadPath(qq);
+    //Tensor* w = Tensor::CreateTensorByLoadPath(qq);
     w->Matmul(q)->PrintData();
     w->PrintData();
 }
