c4b40d199289d3ad1fe03fc27743f749eb7eaa77
takahashiuhiro <takahashiuhiro@outlook.com>
2023-11-05 22:54:00 +0800

    fix bug

--- Code/AutomaticDifferentiation/Ops/PowOps.cpp
+++ Code/AutomaticDifferentiation/Ops/PowOps.cpp
@@ -22,27 +22,17 @@ void PowOps::Backward()
     if(this->CG->GetNode(NodeidList[0])->Property.Get<bool>("RequireGrad"))
     {
         std::string ThisDNodeid = this->CG->GetDNodeid(Nodeid);
-
-
         std::string PowNode = this->CG->GetNodeidByOps(OpsType::Pow, {NodeidList[0]});
         this->CG->RegisterVariableNode(PowNode);
         this->CG->RegisterOpsCompleted(PowNode, {NodeidList[0]}, OpsType::Pow, Dict());
         this->CG->GetCGOps(PowNode)->SetEleExponent(GetEleExponent()-1);
         this->CG->GetCGOps(PowNode)->AfterSettingShapeComputing();
-
-        std::string MulNode = this->CG->GetNodeidByOps(OpsType::EleMul, {ThisDNodeid, Nodeid});
-        this->CG->RegisterVariableNode(MulNode);
-        this->CG->RegisterOpsCompleted(MulNode, {ThisDNodeid, PowNode}, OpsType::EleMul, Dict());
-        this->CG->GetCGOps(MulNode)->AfterSettingShapeComputing();
-
         std::string NewDNode = this->CG->GetDPartNodeid(NodeidList[0], Nodeid);
         this->CG->RegisterVariableNode(NewDNode);
-        this->CG->RegisterOpsCompleted(NewDNode, {MulNode}, OpsType::Add, Dict());
-        this->CG->GetCGOps(NewDNode)->SetAddWeight({{MulNode, GetEleExponent()}});
+        this->CG->RegisterOpsCompleted(NewDNode, {ThisDNodeid, PowNode}, OpsType::EleMul, Dict());
         this->CG->GetCGOps(NewDNode)->AfterSettingShapeComputing();
-
         this->CG->RegisterOpsAddEdge(this->CG->GetDNodeid(NodeidList[0]), NewDNode);
-        this->CG->GetCGOps(this->CG->GetDNodeid(NodeidList[0]))->SetAddWeight({{NewDNode, 1.}});
+        this->CG->GetCGOps(this->CG->GetDNodeid(NodeidList[0]))->SetAddWeight({{NewDNode, GetEleExponent()}});
     }
 }
 
