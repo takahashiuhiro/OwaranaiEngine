6400fa4850b3a75bf301fa8f7feac4cdc455c682
takahashiuhiro <takahashiuhiro@outlook.com>
2024-04-17 02:03:58 +0800

    dynamictensor

--- Code/DynamicAutomaticDifferentiation/DynamicTensor.cpp
+++ Code/DynamicAutomaticDifferentiation/DynamicTensor.cpp
@@ -1,127 +1,50 @@
 #include "DynamicTensor.h"
 
-DynamicOps::DynamicOps(DynamicTensor* DynamicTensorNode){leafNode = DynamicTensorNode;}
 
-DynamicTensor::DynamicTensor(std::shared_ptr<Tensor> InputTensor,bool IsRequiresGrad)
+DynamicTensor::DynamicTensor(){};
+DynamicTensor::DynamicTensor(std::shared_ptr<Tensor> InputTensorPointer, int InputRequiresGrad)
 {
-    TensorPointer = InputTensor;
-    RequiresGrad = IsRequiresGrad;
-    if (IsRequiresGrad)
-    {
-        Grad = std::make_shared<DynamicTensor>(std::shared_ptr<Tensor>(TensorPointer->CopyNewEmptyTensor()),false);
-        Grad->TensorPointer->FillArray(0);
-    }
+	RequiresGrad = InputRequiresGrad;
+	TensorPointer = InputTensorPointer;
+	Ops = std::make_shared<DynamicOps>();//每个动态张量都需要算子
+	Ops->LeafNode = this;//算子连回自家张量的途径
+	if (InputTensorPointer)
+	{
+		auto GradTensor = std::shared_ptr<Tensor>(InputTensorPointer->CopyNewEmptyTensor());
+		GradTensor->FillArray(0);
+		Grad = std::make_shared<DynamicTensor>(GradTensor, InputRequiresGrad-1);
+		Ops->GradOps = Grad->Ops;//算子直接找到导数算子
+	}
 }
-DynamicTensor::DynamicTensor(const DynamicTensor& Other){Set(this, &Other);}
-DynamicTensor DynamicTensor::CreateDynamicTensor(std::shared_ptr<Tensor> InputTensor, bool IsRequiresGrad){return DynamicTensor(InputTensor);}
-DynamicTensor DynamicTensor::CreateDynamicTensor(std::vector<size_t>shape, bool IsRequiresGrad, size_t DeviceNum){return DynamicTensor(std::make_shared<Tensor>(shape, DeviceNum));}
-DynamicTensor DynamicTensor::CreateDynamicTensor(std::vector<size_t>shape, std::vector<float>InputData, bool IsRequiresGrad, size_t DeviceNum){return DynamicTensor(std::make_shared<Tensor>(shape, DeviceNum, InputData));}
-DynamicTensor DynamicTensor::CreateVector(std::vector<float>InputData, size_t DeviceNum)
+
+void DynamicTensor::OpsSetInMap()
 {
-    std::vector<size_t>ThisNewShape = { 1U,InputData.size() };
-    return DynamicTensor(std::make_shared<Tensor>(ThisNewShape, DeviceNum, InputData));
+	ForwardOpsMap[OpsType::Add] = Add;
 }
 
 DynamicTensor::~DynamicTensor()
 {
-    SetOutputList(Ops,this);
-    for (auto& element : OutNodeList)SetInputList(element->Ops, this);
-    if(TensorPointer!=nullptr)
-    {
-        TensorPointer.reset();
-    }
+	Ops->LeafNode = nullptr;//此时算子和动态张量会脱离
 }
 
-void DynamicTensor::SetOutputList(DynamicOps& CurOps, DynamicTensor* TargetOutputNode)
+DynamicTensor DynamicTensor::SetComputationalHistory(Tensor* ResTensor, std::vector<DynamicTensor*>InputList, he InputPrams, size_t InputOpsType, bool RequiresGrad)
 {
-    for (size_t a = 0; a < CurOps.InputOpsList.size(); a++)
-    {
-        if (CurOps.InputOpsList[a].leafNode != nullptr)
-        {
-            for (auto& element : TargetOutputNode->OutNodeList)CurOps.InputOpsList[a].leafNode->OutNodeList.insert(element);
-            CurOps.InputOpsList[a].leafNode->OutNodeList.erase(TargetOutputNode);
-            continue;
-        }
-        SetOutputList(CurOps.InputOpsList[a], TargetOutputNode);
-    }
+	if (!RequiresGrad)return DynamicTensor(std::shared_ptr<Tensor>(ResTensor));
+	int MaxRequiresGrad = 0;
+	for (size_t a = 0; a < InputList.size(); a++)MaxRequiresGrad = std::max(MaxRequiresGrad, InputList[a]->RequiresGrad);
+	DynamicTensor Res = DynamicTensor(std::shared_ptr<Tensor>(ResTensor), MaxRequiresGrad);
+	Res.Ops->DynamicOpsType = InputOpsType;
+	Res.Ops->Params = InputPrams;
+	for (size_t a = 0; a < InputList.size(); a++)
+	{
+		Res.Ops->InputOpsList.push_back(InputList[a]->Ops);
+		InputList[a]->Ops->OutputOpsSet.insert(Res.Ops.get());
+	}
+	return Res;
 }
 
-void DynamicTensor::SetInputList(DynamicOps& CurOps, DynamicTensor* TargetOutputNode)
+DynamicTensor DynamicTensor::Add(std::vector<DynamicTensor*>InputList, he InputPrams, bool RequiresGrad)
 {
-    for (size_t a = 0; a < CurOps.InputOpsList.size(); a++)
-    {
-        if (CurOps.InputOpsList[a].leafNode !=nullptr)
-        {
-            if (CurOps.InputOpsList[a].leafNode == TargetOutputNode)
-            {
-                CurOps.InputOpsList[a] = TargetOutputNode->Ops;
-                CurOps.InputOpsList[a].leafNode = nullptr;
-            }
-        }
-        else SetInputList(CurOps.InputOpsList[a], TargetOutputNode);
-    }
-}
-
-void DynamicTensor::PrintData()
-{
-    TensorPointer->PrintData();
-}
-
-void DynamicTensor::SetForwardHistory(DynamicTensor& InputRes, size_t InputOptType, std::vector<DynamicTensor*>OpsList, he InputPrams, bool IsRequiresGrad)
-{   
-    if (!IsRequiresGrad)return;
-    InputRes.Ops.DynamicOpsType = InputOptType;
-    InputRes.Ops.InputOpsList = {};
-    InputRes.Ops.Params = InputPrams;
-    for (size_t a = 0; a < OpsList.size(); a++)
-    {
-        InputRes.Ops.InputOpsList.push_back(DynamicOps(OpsList[a]));
-        OpsList[a]->OutNodeList.insert(&InputRes);
-        if (OpsList[a]->RequiresGrad)
-        {
-            InputRes.RequiresGrad = true;
-            InputRes.Grad = std::make_shared<DynamicTensor>(std::shared_ptr<Tensor>(InputRes.TensorPointer->CopyNewEmptyTensor()), false);
-            InputRes.Grad->TensorPointer->FillArray(0);
-        }
-    }
-}
-
-void DynamicTensor::Backward(DynamicTensor LossResult)
-{
-
-}
-
-void DynamicTensor::Backward()
-{
-    Grad = std::make_shared<DynamicTensor>(std::shared_ptr<Tensor>(TensorPointer->Copy()));//在这种情况下启动这个函数的时候自己的值就是初始的梯度值
-    std::map<DynamicOps*, std::shared_ptr<DynamicTensor>>GradGlobalResult;//计算完毕的全局导数(存在的意义主要是有些节点没有实体，不能直接存到tensor里)
-    std::map<DynamicOps*, std::shared_ptr<DynamicTensor>>ForwardGlobalResult;//计算完毕的实体
-    std::map<std::pair<DynamicOps*, DynamicOps*>, std::shared_ptr<DynamicTensor>>PartGradGlobalResult;//计算完毕的输入->输出的导数结果(对应一个输入节点多个输出节点的情况)
-    GradGlobalResult[&(this->Ops)] = Grad;
-    BackwardDfs(GradGlobalResult, ForwardGlobalResult, PartGradGlobalResult, &(this->Ops));
-}
-
-void DynamicTensor::BackwardDfs(std::map<DynamicOps*, std::shared_ptr<DynamicTensor>>& GradGlobalResult, std::map<DynamicOps*, std::shared_ptr<DynamicTensor>>& ForwardGlobalResult, std::map<std::pair<DynamicOps*, DynamicOps*>, std::shared_ptr<DynamicTensor>>& PartGradGlobalResult,DynamicOps* CurOps)
-{
-    //todo
-    //1.求实体::如果ops没有实体，那就先dfs(另写一个函数做这个事，结束条件是到一个有实体，或者没有实体但是求过了)求出来实体存在ForwardGlobalResult里
-    for (size_t a = 0; a < CurOps->InputOpsList.size(); a++)
-    {
-    }
-    //2.通过当前这一层已有的求出输入节点到该节点的导数，每次算出来一个部分导数，就检查是不是所有部分都算出来了，如果是那就向上，不是就停止
-    //3.如果碰到不需要算导数的那就不用求
-    //4.无论是否有实体都要在map里存一份，有实体的还要额外在tensor里存
-}
-
-void DynamicTensor::Set(DynamicTensor* ThisTensor, const DynamicTensor* OtherTensor)
-{
-    ThisTensor->TensorPointer = OtherTensor->TensorPointer;
-    ThisTensor->Ops = OtherTensor->Ops;
-}
-
-DynamicTensor DynamicTensor::Add(std::vector<DynamicTensor*>InputList, he InputPrams, bool IsRequiresGrad)
-{
-    DynamicTensor Res = DynamicTensor(std::shared_ptr<Tensor>(InputList[0]->TensorPointer->Add(InputList[1]->TensorPointer.get())));
-    SetForwardHistory(Res, OpsType::Add, InputList, InputPrams, IsRequiresGrad);
-    return Res;
+	auto ResTensorContent = InputList[0]->TensorPointer->Add(InputList[1]->TensorPointer.get());
+	return SetComputationalHistory(ResTensorContent, InputList, InputPrams,OpsType::Add, RequiresGrad);
 }
--- Code/DynamicAutomaticDifferentiation/DynamicTensor.h
+++ Code/DynamicAutomaticDifferentiation/DynamicTensor.h
@@ -13,15 +13,13 @@ class DynamicTensor;
 
 struct DynamicOps
 {
-    DynamicOps(){}
-    DynamicOps(DynamicTensor* DynamicTensorNode);
-    /**算子，叶子节点为Base.*/
-    size_t DynamicOpsType;
-    /**只有叶子节点有这一项.*/
-    DynamicTensor* leafNode = nullptr;
-    /**输入节点.*/
-    std::vector<DynamicOps>InputOpsList;
-    he Params;
+    /**算子成员变量.*/
+    he Params;//算子参数
+    size_t DynamicOpsType;//算子类型，叶子节点为Base
+    DynamicTensor* LeafNode = nullptr;//指向算子的结算张量，如果为nullptr代表张量被删除
+    std::vector<std::shared_ptr<DynamicOps>>InputOpsList;//输入节点，需要后继保存输入的资源
+    std::set<DynamicOps*>OutputOpsSet;//输出节点，不需要保存output的资源
+    std::shared_ptr<DynamicOps>GradOps = nullptr;
 };
 
 class DynamicTensor
@@ -29,42 +27,28 @@ class DynamicTensor
 public:
 
     /**动态张量的成员变量.*/
-    std::shared_ptr<Tensor> TensorPointer = nullptr;
-    DynamicOps Ops;
-    std::set<DynamicTensor*>OutNodeList;//输出节点，只有在需要求导的时候可用
-    bool RequiresGrad = 0;
+    std::shared_ptr<Tensor> TensorPointer = nullptr;//存的张量内容
+    std::shared_ptr<DynamicOps>Ops = nullptr;//每个动态张量的算子，如果张量被删掉但是需要计算图，这个算子可以交出去，交出去的时候需要删掉算子中的leafNode变量为nullptr
+    int RequiresGrad = 0;
     std::shared_ptr<DynamicTensor>Grad = nullptr;
+    std::map<size_t, DynamicTensor(*)(std::vector<DynamicTensor*>, he, bool)>ForwardOpsMap;
+    //std::map<size_t, DynamicTensor(*)(std::vector<DynamicTensor*>, he, bool)>BackwardOpsMap;应该有，但可能不是这个类型
 
-    /**初始化动态张量.*/
-    DynamicTensor(){};
-    DynamicTensor(std::shared_ptr<Tensor> InputTensor, bool IsRequiresGrad = false);
-    DynamicTensor(const DynamicTensor& Other);
+    /**内存管理.*/
+    DynamicTensor();//初始化动态张量
+    DynamicTensor(std::shared_ptr<Tensor> InputTensorPointer, int InputRequiresGrad = 0);
+    void OpsSetInMap();
 
-    /**创建动态张量.*/
-    static DynamicTensor CreateDynamicTensor(std::shared_ptr<Tensor> InputTensor, bool IsRequiresGrad = false);
-    static DynamicTensor CreateDynamicTensor(std::vector<size_t>shape, bool IsRequiresGrad = false, size_t DeviceNum = 0);
-    static DynamicTensor CreateDynamicTensor(std::vector<size_t>shape, std::vector<float>InputData, bool IsRequiresGrad = false, size_t DeviceNum = 0);
-    /**创建向量，默认为行向量.*/
-    static DynamicTensor CreateVector(std::vector<float>InputData, size_t DeviceNum = 0);
-
-    /**析构函数释放内存.*/
-    ~DynamicTensor();
-    void SetOutputList(DynamicOps&CurOps,DynamicTensor* TargetOutputNode);
-    void SetInputList(DynamicOps& CurOps, DynamicTensor* TargetOutputNode);
+    ~DynamicTensor();//析构函数释放内存
 
     /**Tensor内函数组装.*/
-    void PrintData();
 
     /**运算符重载.*/
 
     /**计算图逻辑.*/
-    static void SetForwardHistory(DynamicTensor&InputRes ,size_t InputOptType, std::vector<DynamicTensor*>OpsList,he InputPrams, bool IsRequiresGrad);
-    void Backward(DynamicTensor LossResult);//输入一个反向向量开始反传
-    void Backward();//输入一个反向向量开始反传,这是起点位置
-    void BackwardDfs(std::map<DynamicOps*, std::shared_ptr<DynamicTensor>>& GradGlobalResult, std::map<DynamicOps*, std::shared_ptr<DynamicTensor>>&ForwardGlobalResult, std::map<std::pair<DynamicOps*, DynamicOps*>, std::shared_ptr<DynamicTensor>>& PartGradGlobalResult, DynamicOps* CurOps);
-    
+    static DynamicTensor SetComputationalHistory(Tensor* ResTensor, std::vector<DynamicTensor*>InputList, he InputPrams,size_t InputOpsType, bool RequiresGrad);
+
     /**运算符重载逻辑.*/
-    void Set(DynamicTensor* ThisTensor, const DynamicTensor* OtherTensor);
 
     /**算子.*/
     static DynamicTensor Add(std::vector<DynamicTensor*>InputList, he InputPrams, bool RequiresGrad = false);
