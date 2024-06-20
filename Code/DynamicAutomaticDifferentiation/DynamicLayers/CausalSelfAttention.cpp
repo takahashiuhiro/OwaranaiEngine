9b1621540e5bf6f2f9c38dfbfb593b0ac85f6be4
takahashiuhiro <takahashiuhiro@outlook.com>
2024-06-20 23:52:47 +0800

    add self attention

--- Code/DynamicAutomaticDifferentiation/DynamicLayers/CausalSelfAttention.cpp
+++ Code/DynamicAutomaticDifferentiation/DynamicLayers/CausalSelfAttention.cpp
@@ -1,13 +1,28 @@
 #include "CausalSelfAttention.h"
+#include "Linear.h"
 
 void CausalSelfAttention::SetLayerParams()
 {
-    
+    NHead = Params["NHead"].i();
+    NEmbd = Params["NEmbd"].i();
+    Dropout = Params["Dropout"].f();
+    BlockSize = Params["BlockSize"].i();
+    Bias = Params["Bias"].i();
 }
 
 void CausalSelfAttention::InitContent()
 {
-
+    he CAttnParams = he::NewDict();
+    CAttnParams["InFeatures"] = NEmbd;
+    CAttnParams["OutFeatures"] = 3*NEmbd;
+    CAttnParams["Bias"] = Bias;
+    CreateNewLayer<Linear>("CAttn", CAttnParams);
+    he CProJParams = he::NewDict();
+    CProJParams["InFeatures"] = 3*NEmbd;
+    CProJParams["OutFeatures"] = NEmbd;
+    CProJParams["Bias"] = Bias;
+    CreateNewLayer<Linear>("CProj", CProJParams);
+    
 }
 
 std::vector<DynamicTensor> CausalSelfAttention::Forward(std::vector<DynamicTensor>InputForwardList, he InputParams)
--- Code/DynamicAutomaticDifferentiation/DynamicLayers/CausalSelfAttention.h
+++ Code/DynamicAutomaticDifferentiation/DynamicLayers/CausalSelfAttention.h
@@ -6,6 +6,11 @@ class CausalSelfAttention : public BaseDynamicLayer
     virtual void SetLayerParams();
 	virtual void InitContent();
 	virtual std::vector<DynamicTensor> Forward(std::vector<DynamicTensor>InputForwardList, he InputParams = he());
-
+ 
+	int NHead;
+	int NEmbd;
+	float Dropout;
+	int BlockSize;
+	int Bias;
     
 };
