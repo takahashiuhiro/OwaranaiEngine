fix split bug finish causalselfattention test fix softmax bug fix transpose bug

test.py
@@ -7,22 +7,75 @@ import typing
 import random
 import tqdm
 
+class CausalSelfAttention(nn.Module):
 
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        # regularization
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.dropout = config.dropout
+        # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                        .view(1, 1, config.block_size, config.block_size))
 
-q = torch.Tensor([1,2,3,4,5,6,7,8,9.])
-q = q.view([3,3])
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
 
-w = torch.Tensor([1,2,3,4,5,6,7,8,9.])
-w = w.view([3,3])
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        qkv = self.c_attn(x)
+        q, k, v  = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        #print(k.transpose(-2, -1))
 
-q.requires_grad = True
+        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
 
-mmsk = torch.ones([3,3],dtype = torch.bool).tril()
+        # manual implementation of attention
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, -1e9)
+        att = F.softmax(att, dim=-1)
 
-qq = q.masked_fill(mmsk, 99)
+        att = self.attn_dropout(att)
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        #return y
+        # output projection
+        y = self.resid_dropout(self.c_proj(y))
+        return y
 
-qq.backward(w)
+class config(object):
+    def __init__(self):
+        self.dropout = 0
+        self.n_embd =6
+        self.n_head = 3
+        self.bias = 0
+        self.block_size = 40
 
-print(qq)
-print(q.grad)
+cfg = config()
+ly = CausalSelfAttention(cfg)
 
+with torch.no_grad():  # 禁用梯度计算
+    ly.c_attn.weight = nn.Parameter(torch.ones(ly.c_attn.weight.shape))
+    ly.c_proj.weight = nn.Parameter(torch.ones(ly.c_proj.weight.shape)*1.5)
+
+#print(ly.c_attn.weight)
+#print(ly.c_proj.weight)
+
+x = torch.ones([4,5,6])
+x.requires_grad = True
+
+y = ly(x).sum()
+
+y.backward()
+
+print(y)
+print(x.grad)