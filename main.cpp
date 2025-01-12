ea851f8a68d01cb79717614d8c0316d36798e3b2
takahashiuhiro <takahashiuhiro@outlook.com>
2025-01-12 17:08:47 +0800

    fix cmake

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main() 
 {
-    Tensor* a = new Tensor({2,2}, 0, {1,2,3,4});
-    Tensor* b = new Tensor({2,2}, 0, {4,3,2,1});
+    Tensor* a = new Tensor({2,2}, 1, {1,2,3,4});
+    Tensor* b = new Tensor({2,2}, 1, {4,3,2,1});
     DynamicTensor dta(std::shared_ptr<Tensor>(a), 1);
     DynamicTensor dtb(std::shared_ptr<Tensor>(b), 1);
     auto dtc = (dta%dtb).Sum();
