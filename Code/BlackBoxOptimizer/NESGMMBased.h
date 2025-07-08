ebf79d89261450d1e3b3b56f0c34a94361ea60ff
takahashiuhiro <takahashiuhiro@outlook.com>
2025-07-08 13:17:28 +0800

    update sth

--- Code/BlackBoxOptimizer/NESGMMBased.h
+++ Code/BlackBoxOptimizer/NESGMMBased.h
@@ -1,13 +1,6 @@
 #pragma once
 #include "BaseBlackBoxOptimizer.h"
 
-// 标准高斯分布
-struct GaussianDistribution
-{
-    DynamicTensor Mean;
-    DynamicTensor Var;
-};
-
 struct GMM
 {
     DynamicTensor Mean;
@@ -16,32 +9,34 @@ struct GMM
 };
 
 
-
 template<typename TargetType>
 struct NESGMMBased: public BaseBlackBoxOptimizer<TargetType>
 {
 
     int DimNum; //优化变量维度
     int SampleNum; //采样数
-    int CosmosNum; //粒子数
+    int CosmosNum; //GMM的高斯数
     double LearingRate_Var; //学习率
     double LearingRate_Mean; //学习率
 
+    GMM TargetDistribution; // 目标分布
 
+    bool IsWarmStart = false; // 是不是已经热启动了，如果热启动了就可以跳过随机初始化
 
     virtual void ParamsInit()
     {
         DimNum = this->Params["DimNum"].i();
         SampleNum = this->Params.DictGet("SampleNum", 30).i();
-        CosmosNum = this->Params.DictGet("CosmosNum", 30).i();
+        CosmosNum = this->Params.DictGet("CosmosNum", 1).i();
         LearingRate_Mean = this->Params.DictGet("LearingRate_Mean", 0.1).f();
         LearingRate_Var = this->Params.DictGet("LearingRate_Var", 0.02).f();
     }
 
+    
+
     virtual DynamicTensor Solve()
     {        
-        // 暂时返回值
-        return DynamicTensor();
+        
     }
     
 };
